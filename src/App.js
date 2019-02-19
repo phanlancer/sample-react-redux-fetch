0fe744fe4ef0771412ad9e03bce2e782d255818f
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-02-19 20:03:44 +0700

    title of the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import PokemonList from './scenes/PokemonList';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import PokemonList from "./scenes/PokemonList";
 
 class App extends Component {
   render() {
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Sample React Redux fetch</h1>
         </header>
         <PokemonList />
       </div>
