d4048242bfc7b00ca52424866d597deee2bee622
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-07-02 22:39:06 +0700

    update root store

--- src/services/store/index.js
+++ src/services/store/index.js
@@ -1,20 +1,16 @@
-import {createStore, applyMiddleware} from 'redux'
-import {middleware as fetchMiddleware} from 'react-redux-fetch'
+import { createStore, applyMiddleware } from "redux";
+import { middleware as fetchMiddleware } from "react-redux-fetch";
 
-import reducer from '../reducers';
+import reducer from "../reducers";
 
-function configureStore () {
-
-  const store = createStore(
-    reducer,
-    applyMiddleware(fetchMiddleware)
-  )
+function configureStore() {
+  const store = createStore(reducer, applyMiddleware(fetchMiddleware));
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
+    module.hot.accept("../reducers", () => {
       // We need to require for hot reloading to work properly.
-      const nextReducer = require('../reducers'); // eslint-disable-line global-require
+      const nextReducer = require("../reducers"); // eslint-disable-line global-require
       store.replaceReducer(nextReducer);
     });
   }
